a9fda1d32500d093c9849e6a03584241d40670b5
shivam sharma <shivam.sh9557@gmail.com>
2020-04-10 13:40:35 +0530

    Fibonacci series added

--- scripts/01_fibonacci.py
+++ scripts/01_fibonacci.py
@@ -1,1 +1,31 @@
+## fibibonacci numbers: 0 1 1 2 3 5 8 13 21 34 ....
 
+## Using lambda function 
+n = int(input(""))
+fib = [0,1]
+
+list(map(lambda i: fib.append(fib[i-1] + fib[i-2]), range(2, n)))
+print("Fibonacci numbers:" , fib)
+
+
+
+
+
+## using list comprehension
+n = int(input(""))
+fib = [0,1]
+
+[fib.append(fib[i-1] + fib[i-2]) for i in range(2,n)]
+print("Fibonacci numbers:" , fib)
+
+
+
+
+
+## using loop 
+n = int(input(""))
+fib = [0,1]
+
+for i in range(2, n):
+    fib.append(fib[i-1] + fib[i-2])
+print("Fibonacci numbers:" , fib)
--- scripts/hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello World!")
-print("Hi i am a programmer.")
-print("I am now working on python.")
