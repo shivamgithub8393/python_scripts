9deb9763b8dc8c85e0d088a9e0e17faae3c9e15c
shivam sharma <shivam.sh9557@gmail.com>
2020-04-10 12:52:25 +0530

    circle and hello scripts added

--- circle.py
+++
@@ -1,6 +0,0 @@
-r=16
-perimeter=2*3.14*r
-area=3.14*r*r
-print("radius of the circle:", r)
-print("perimeter of circle:",perimeter)
-print("area of circle:", area)
---
+++ scripts/circle.py
@@ -0,0 +1,6 @@
+r=16
+perimeter=2*3.14*r
+area=3.14*r*r
+print("radius of the circle:", r)
+print("perimeter of circle:",perimeter)
+print("area of circle:", area)
